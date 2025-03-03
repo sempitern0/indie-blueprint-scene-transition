844e38d61fc476c6ffa7f773ab3fd2e4212a5278
ninetailsrabbit <danieldigitalspace@proton.me>
2025-03-03 11:40:28 +0000

    returned the test scene to use dissolve transitoin

--- test_scenes/test.gd
+++ test_scenes/test.gd
@@ -6,8 +6,8 @@ func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		IndieBlueprintSceneTransitioner.transition_to(
 			next_scene,
-			IndieBlueprintPremadeTransitions.ColorFade,
-			IndieBlueprintPremadeTransitions.ColorFade,
+			IndieBlueprintPremadeTransitions.Dissolve,
+			IndieBlueprintPremadeTransitions.Dissolve,
 			{ 	"in": {"texture": IndieBlueprintPremadeTransitions.HorizontalPaintBrush, "duration": 1.0, "color": Color.LIGHT_SKY_BLUE},
 				"out": {"texture": IndieBlueprintPremadeTransitions.VerticalPaintBrush, "duration": 1.5, "color": Color.LIGHT_SEA_GREEN,}
 			}

c436beb6c5301ff57aef1c8b762f792d07e76e42
ninetailsrabbit <danieldigitalspace@proton.me>
2025-03-04 10:22:20 +0000

    updated plugin to godot 4.4

--- test_scenes/test.gd
+++ test_scenes/test.gd
@@ -2,13 +2,13 @@ extends Node2D
 
 @export var next_scene: PackedScene
 
-func _unhandled_input(event: InputEvent) -> void:
+func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		IndieBlueprintSceneTransitioner.transition_to(
 			next_scene,
 			IndieBlueprintPremadeTransitions.Dissolve,
 			IndieBlueprintPremadeTransitions.Dissolve,
-			{ 	"in": {"texture": IndieBlueprintPremadeTransitions.Curtains, "duration": 1.0, "color": Color.BLACK},
-				"out": {"texture": IndieBlueprintPremadeTransitions.Curtains, "duration": 1.5, "color": Color.BLACK,}
+			{ 	"in": {"texture": IndieBlueprintPremadeTransitions.VerticalPaintBrush, "duration": 1.0, "color": Color.BLACK},
+				"out": {"texture": IndieBlueprintPremadeTransitions.VerticalPaintBrush, "duration": 1.5, "color": Color.BLACK,}
 			}
 		)
